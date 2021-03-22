7d7e1860b369397d6a3fe0222b51bc08d4471b56
Tobias Keller <kellertobs@gmail.com>
2021-03-22 10:04:10 +0100

    updated exercise on implicit T-solve

--- exercise3_implicitParameters.m
+++ exercise3_implicitParameters.m
@@ -1,13 +1,15 @@
-% heat1Dexplicit.m
-%
-% Solves the 1D heat equation with an explicit finite difference scheme
+%These are the parameters for exercise 3.
+%They should be substitued into execise 2
+%https://www.blogs.uni-mainz.de/fb09-geosciences/files/2018/12/Finite_Differerence_Timpl_expl.pdf
 
 clear
 
 % Physical parameters
 L       =   100;         % Length of modeled domain     [m]
 Tmagma  =   1200;        % Temperature of magma         [C]
-Trock   =   300;         % Temperature ofcountry rock   [C]
+Trock   =   300;         % Temperature of country rock   [C]
+Tleft   =   200;         % Temperature of left boundary [C]
+Tright  =   100;         % Temperature of right boundary [C]
 kappa   =   1e-6;        % Thermal diffusivity of rock  [m2/s]
 W       =   5;           % Width of dike                [m]
 day     =   3600*24;     % # seconds per day
@@ -16,7 +18,7 @@ dt      =   1*day;       % Timestep                     [s]
 
 % Numerical parameters
 nx      =   201;         % Number of gridpoints in x-direction
-nt      =   50;         % NUmber of timesteps to compute
+nt      =   500;         % NUmber of timesteps to compute
 dx      =   L/(nx-1);     % Spacing of grid
 x       =   -L/2:dx:L/2; % Grid
 
@@ -24,26 +26,41 @@ x       =   -L/2:dx:L/2; % Grid
 T       =   ones(size(x))*Trock;
 T(abs(x)<=W/2) = Tmagma;
 
+% set boundary values
+T(x == -L/2) = Tleft;
+T(x ==  L/2) = Tright;
+
 figure(1); clf;
 figure(2); clf;
 
 time = 0;
 Trecord = zeros(nt,1);
 for n=1:nt               % Timestep loop
-    % Compute new temperature
-    Tnew   =    zeros(1,nx);
-    for i=2:nx-1
-        Tnew(i) =  T(i) + kappa*dt/dx^2 * (T(i+1)-2*T(i)+T(i-1));
+
+    s = kappa * dt / dx^2;   % Heat equation
+    
+    % Coefficient matrix A
+    A = sparse(nx,nx);
+    for i = 2:nx-1
+        A(i, i-1)  = -s;
+        A(i, i  )  = (1+2*s);
+        A(i, i+1)  = -s;
     end
     
+    % boundary conditions
+    A(1,1)   = 1;
+    A(nx,nx) = 1;
     
-    % Set boundary conditions
-    Tnew(1)     =   T(1);
-    Tnew(nx)    =   T(nx);
+    rhs = zeros(nx,1);
+    rhs(2:nx-1) = T(2:nx-1);
+    rhs(1)      = Tleft;
+    rhs(nx)     = Tright;
     
-    % Update temperature and time
-    T           =   Tnew;
-    time        =   time+dt;
+    % solve linear system of equations
+    T = A\rhs;
+
+    % update time
+    time  =  time+dt;
     
     % Record T at 5 m distance
     Trecord(n) = T(x==5);
@@ -51,7 +68,7 @@ for n=1:nt               % Timestep loop
     if ~mod(n,10)
         %Plot solution
         figure(1), clf
-        plot(x,Tnew);
+        plot(x,T);
         xlabel('x [m]')
         ylabel('Temperature [^oC]')
         title(['Temperature evolution after ',num2str(time/day),'days'])
